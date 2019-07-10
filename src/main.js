9373e82e091084b476e32b17e6b2e30bf3d515bb
ysx <15321451930@163.com>
2019-07-10 22:48:35 +0800

    添加bootstrap框架

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import $ from 'jquery'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap'
 
 Vue.config.productionTip = false
+Vue.use($)
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,22 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <button class="btn btn-primary">测试按钮</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+$(function(){
+    alert(123)
+})
 export default {
   name: 'home',
   components: {
